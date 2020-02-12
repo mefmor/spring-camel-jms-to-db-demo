b739944807aa6e6e1bbee05153d790eda3a2fb08
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-02-12 15:23:00 +0300

    Add maven project

---
+++ src/main/java/net/mefmor/demo/spring/camel/SpringCamelJmsToDbDemoApplication.java
@@ -0,0 +1,13 @@
+package net.mefmor.demo.spring.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamelJmsToDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCamelJmsToDbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/mefmor/demo/spring/camel/SpringCamelJmsToDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mefmor.demo.spring.camel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCamelJmsToDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
